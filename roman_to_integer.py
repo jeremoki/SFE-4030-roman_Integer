41740983c88f115ed8af9720b3b5be3a1ca79a8d
jeremoki <92101241+jeremoki@users.noreply.github.com>
2023-10-16 17:52:25 +0300

    Add files via upload

---
+++ roman_to_integer.py
@@ -0,0 +1,65 @@
+def roman_to_integer(s):
+
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for char in s[::-1]:
+        value = roman_dict[char]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+
+    return total
+
+# Test cases
+test_cases = [
+#Single letters
+    ("I", 1),
+    ("V", 5),
+    ("X", 10),
+    ("L", 50),
+    ("C", 100),
+    ("D", 500),
+    ("M", 1000),
+#Multiple letters
+    ("XI", 11),
+    ("IV", 4),
+    ("XIV", 14),
+    ("III", 3),
+#Repetition
+    ("II", 2),
+
+    ("III", 3),
+
+# iNvalid
+    ("Z", "Error"),
+# Invalid and Valid Letters
+
+("XIZ", "Error"),
+
+# Not Valid
+
+    ("VV", "Error"),
+# Null input
+    ("", "Error"),
+
+
+]
+
+# Run the test cases and validate the results
+for roman_numeral, expected_result in test_cases:
+    result = roman_to_integer(roman_numeral)
+    print(f"The integer equivalent of {roman_numeral} is {result}. Expected: {expected_result}")
+    assert result == expected_result, f"Test failed for {roman_numeral}"
